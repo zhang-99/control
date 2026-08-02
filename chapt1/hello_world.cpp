f4c073f8e5946da81d1e21df91084c30257e70e2
zhang-99 <56794785+zhang-99@users.noreply.github.com>
2026-08-02 22:39:57 +0800

    Update hello_world.cpp

--- chapt1/hello_world.cpp
+++ chapt1/hello_world.cpp
@@ -1,6 +1,10 @@
 #include "iostream"
 int main()
 {
-    std::cout << "Hello World !" << std::endl;
+
+        std::cout << "Hello World !" << std::endl;
+    double aaa,afff,dfdfsfd;
+    aa=0.0;
+    afff=4.0;
     return 0;
 }
